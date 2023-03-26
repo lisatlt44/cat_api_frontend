316185dc185c3c5a53aa469df435aabe04526b6a
lisatlt44 <lisa.tallet1@gmail.com>
2023-03-26 17:20:10 +0200

    Reorganisation propre des composants

--- src/App.js
+++ src/App.js
@@ -10,37 +10,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-// import { useState, useEffect } from 'react';
-
-// const CatPage = () => {
-//   const [catData, setCatData] = useState(null);
-
-//   useEffect(() => {
-//     const loadData = async () => {
-//       const response = await fetch('https://api.thecatapi.com/v1/images/search?has_breeds=1&api_key=live_mtl9hrEB87yKSbY11iXEkgFZtnsMeZtj2VhYQIJkQNOnRwIJkMleFZlmgHLyDF7p');
-//       const data = await response.json();
-//       setCatData(data[0]);
-//     };
-
-//     loadData();
-//   }, []);
-
-//   return (
-//     <div>
-//       {catData && (
-//         <div>
-//           <img src={catData.url} alt="A cat" />
-//           <button onClick={() => setCatData()}>Refresh</button>
-//           <div>Race : {catData.breeds[0].name}</div>
-//           <div>Origine : {catData.breeds[0].origin}</div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default CatPage;
